013e3539daa2663b077b81a39233d198a6547d4b
Shivamani1225 <shivamani2410@gmail.com>
2024-09-01 13:20:07 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], i};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    int n, target;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    vector<int> nums(n);
+    cout << "Enter " << n << " integers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    
+    cout << "Enter the target sum: ";
+    cin >> target;
+    
+    vector<int> result = twoSum(nums, target);
+    if (result.size() == 2) {
+        cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+    return 0;
+}
